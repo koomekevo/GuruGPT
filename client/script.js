37c4277aa0aa5c2b86ac7798941b86fb6bcda8e7
koomekevo <koome.kevo@gurucrafts.net>
2023-02-09 15:57:21 +0300

    Add chatStripe

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-import bot from "./assets/bot.svg";
+dimport bot from "./assets/bot.svg";
 import user from "./assets/user.svg";
 
 const form = document.querySelector("form");
@@ -37,4 +37,19 @@ function generateUniqueId() {
   const hexadecimalString = randomNumber.toString(16);
 
   return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe = (isAi, value, uniqueId) {
+  return (
+    `
+      <div class="wrapper ${isAi && 'ai'}">
+        <div class="chat>
+          <div className="profile">
+            <img src="${isAi ? bot : user}" alt="{isAi ? 'bot' : 'user'}" />
+          <div>
+          <div class="message" id=${uniqueId}>${value}</div>
+        </div>
+      </div>   
+    `
+  )
 }
