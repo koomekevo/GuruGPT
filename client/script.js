f9818828e072b10329dc2248d0ce30fe2724bde8
koomekevo <koome.kevo@gurucrafts.net>
2023-02-09 19:11:10 +0300

    Update fetch URL

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://gurugpt.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
