c3630fcb459298f85b30218766eed177f8b443fa
koomekevo <koome.kevo@gurucrafts.net>
2023-02-12 13:02:50 +0300

    Change model from DaVinci to Curie

--- server/server.js
+++ server/server.js
@@ -26,10 +26,10 @@ app.post("/", async (req, res) => {
     const prompt = req.body.prompt;
 
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: "text-curie-001",
       prompt: `${prompt}`,
       temperature: 0,
-      max_tokens: 3000,
+      max_tokens: 2048,
       top_p: 1,
       frequency_penalty: 0.5,
       presence_penalty: 0,
